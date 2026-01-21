98ae20d6b66db4f559a63b83379f884bf2b789c8
Sandun Gunathilaka <sandungunathilaka2002@gmail.com>
2026-01-21 16:40:18 +0530

    bug fix

--- app.py
+++ app.py
@@ -3,29 +3,26 @@ from transformers import pipeline
 
 st.set_page_config(page_title="AI Summarization Tool")
 
-
 @st.cache_resource
 def load_model():
     return pipeline("summarization", model="facebook/bart-large-cnn")
 
-
-summarizer_modal = load_model()
+summarizer_model = load_model()
 
 st.title("Text Summarization Tool")
-coll, col2 = st.columns([2, 1])
-
-with coll:
-    user_input = st.text_area("Enter text to summarize", height=200)
-    summarizer_button = st.button("Summarizer Text", type="primary")
+col1, col2 = st.columns([2,1])
+with col1:
+    user_input =st.text_area("Enter text to summarize", height=200)
+    summarizer_button = st.button("Summarize Text", type="primary")
 
 with col2:
-    st.markdown("Powered by Sandun Gunathilake")
+    st.markdown("Powered by C Clarke Institute Students")
 
 if summarizer_button and user_input:
-    with st.spinner("Summarizing ..."):
-        result = summarizer_modal(user_input)
+    with st.spinner("Summarizing......"):
+        result = summarizer_model(user_input)
         summary_text = result[0]['summary_text']
         st.markdown(summary_text)
 
 elif summarizer_button:
-    st.warning("Please enter text to summarize.")
+    st.warning("Please Enter some text to summarize")
